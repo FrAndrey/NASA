5874065964aae4d9c0d306b76d0dac94c64ab82a
FrAndrey <saspan@inbox.ru>
2023-04-21 16:18:55 -0700

    Base setup: configured the non-storyboard settings, added navigation controller with a port of entry, created basic folder structure.

---
+++ NASA/Services/ImageManager.swift
@@ -0,0 +1,15 @@
+//
+//  ImageManager.swift
+//  NASA
+//
+//  Created by Administrator on 2023-04-21.
+//
+
+import UIKit
+
+struct ImageManager {
+    
+    
+    
+    
+}
---
+++ NASA/View/CollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewController.swift
+//  NASA
+//
+//  Created by Administrator on 2023-04-21.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .orange
+    }
+    
+    
+    
+}
